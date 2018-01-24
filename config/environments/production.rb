f1145b376b95653252eacc37e7317eb86b77ba29
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-01-24 15:04:06 +0900

    test commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true　#画像が読まれないからfalseから変更
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
